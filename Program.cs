66aba3e58babfb07dd183dc290978884d10ce6fd
elifmacit <72386043+elifmacit@users.noreply.github.com>
2021-01-23 20:50:21 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+                DictionaryList<string> isimler = new DictionaryList<string>();
+                isimler.Add("Elif");
+
+
+                Console.WriteLine("Hello World!");
+            }
+
+
+
+        }
+    }
+}
